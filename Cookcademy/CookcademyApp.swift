ebca51c62cfc24e7bcc6f64cb1d033c1112428e9
Hannah Williams <hannahw1ll1ams@yahoo.com>
2024-01-14 22:21:24 +0000

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Hannah Williams on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
